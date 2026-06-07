97893fd1fbbd74e92a32ccfd4e3b5f3ab232d41a
Elian <Elian.E.K@hotmail.com>
2026-06-07 17:57:31 -0300

    ft(Test): -Create ConocimientoTest -Add comentario nuevo para definicion/parametros de pruebas

--- src/test/java/com/example/demo/controller/ConocimientoControllerTest.java
+++ src/test/java/com/example/demo/controller/ConocimientoControllerTest.java
@@ -1,4 +1,10 @@
 package com.example.demo.controller;
 
 public class ConocimientoControllerTest {
+
+    /// TODO: Implementar pruebas unitarias para el ConocimientoController utilizando Mockito y JUnit.
+    /// usar @WebMvcTest para probar el controlador de manera aislada, y @MockBean para simular las dependencias del servicio.
+    /// Asegurarse de cubrir casos como la creación, actualización, eliminación y recuperación de conocimientos, así como la gestión de excepciones.
+   /// usa test del proyecto mascotas como guia para generar las pruebas unitarias, adaptando los casos de prueba
+    /// a las funcionalidades específicas del ConocimientoController.
 }
