2c12baf2e68f6aeb260f5624cc10e2c56fd40fd5
Elian <Elian.E.K@hotmail.com>
2026-08-19 17:47:37 -0300

    ft(Properties): -Create new api-key (ImgBB)

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig {
 
   private static final String[] WHITE_LIST = {
       "/api/v1/auth/**", "/api/v1/**" , "/api/v1/test/**",
-      "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/health"
+      "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/health","/api/v1/health"
   };
 
   public SecurityConfig(JwtAuthFilter jwtAuthFilter, UserDetailsService userDetailsService) {
