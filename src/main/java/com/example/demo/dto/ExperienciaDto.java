3477980e5fee6d27a2c043e8892f3eaa1b2b631e
Elian <Elian.E.K@hotmail.com>
2026-09-02 12:00:19 -0300

    ft(Experiencia):-Chore modificar DTO -Chore cambiar atributo unico tecnolofiaUsada a lista

--- src/main/java/com/example/demo/dto/ExperienciaDto.java
+++ src/main/java/com/example/demo/dto/ExperienciaDto.java
@@ -2,16 +2,11 @@ package com.example.demo.dto;
 
 import com.example.demo.enums.TecnologiaUsada;
 import com.example.demo.enums.TipoExperiencia;
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -21,14 +16,13 @@ public class ExperienciaDto {
 
     private Integer id;
     private String titulo;
-
     private LocalDate fechaInicioProyecto;
     private LocalDate fechaFinProyecto;
     private String descripcion;
     private String link;
-
     private ImagenDto imagen;
     private TipoExperiencia tipoExperiencia;
-    private TecnologiaUsada tecnologiaUsada;
 
+    // 🔥 CAMBIO PRINCIPAL: de Enum a List<Enum>
+    private List<TecnologiaUsada> tecnologiasUsadas;
 }
